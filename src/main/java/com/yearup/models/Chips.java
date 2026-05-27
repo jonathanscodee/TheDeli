9cec66f90f635cd03477bbe9b31729da37fffbf0
jonathan <274123470+jonathanscodee@users.noreply.github.com>
2026-05-27 12:29:34 -0700

    Chips.java implemented

--- src/main/java/com/yearup/models/Chips.java
+++ src/main/java/com/yearup/models/Chips.java
@@ -1,4 +1,22 @@
 package com.yearup.models;
 
-public class Chips {
+public class Chips extends Product{
+
+    public Chips(String name, double price) {
+        super(name, 0);
+    }
+
+
+    @Override
+    public double calculateProductTotal() {
+        return 1.50;
+    }
+
+    @Override
+    public String productDetails() {
+        return getName() + " $" + String.format("%.2f", calculateProductTotal());
+    }
+
+
+
 }
