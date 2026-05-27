32ce6f48fade903b36239b34f8bca88ba4096919
jonathan <274123470+jonathanscodee@users.noreply.github.com>
2026-05-27 13:19:05 -0700

    initial commit for OtherTopping.java

--- src/main/java/com/yearup/models/enums/OtherTopping.java
+++ src/main/java/com/yearup/models/enums/OtherTopping.java
@@ -1,5 +1,31 @@
 package com.yearup.models.enums;
 
 public enum OtherTopping {
-    STEAK(Type.MEAT)
+    STEAK(Type.MEAT),
+    HAM(Type.MEAT),
+    SALAMI(Type.MEAT),
+    ROAST_BEEF(Type.MEAT),
+    CHICKEN(Type.MEAT),
+    BACON(Type.MEAT),
+
+    // Cheeses
+    AMERICAN_CHEESE(Type.CHEESE),
+    SWISS_CHEESE(Type.CHEESE),
+    PROVOLONE_CHEESE(Type.CHEESE),
+    CHEDDAR_CHEESE(Type.CHEESE);
+
+    private final Type type;
+
+    OtherTopping(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        MEAT,
+        CHEESE
+    }
 }
