ae2a734ef9877ac96ca61db9650195d0351cbe7c
jonathan <274123470+jonathanscodee@users.noreply.github.com>
2026-05-27 19:44:28 -0700

    Sandwich.java: completed formatting and property implementation

--- src/main/java/com/yearup/models/Sandwich.java
+++ src/main/java/com/yearup/models/Sandwich.java
@@ -16,7 +16,7 @@ public class Sandwich extends Product{
     private boolean extraCheese;
 
 
-    public Sandwich(String name, double price, SandwichSize size, BreadType breadType, List<IncludedTopping> includedTopping, List<OtherTopping> otherTopping, List<Sauce> sauces, boolean isToasted, boolean extraMeat, boolean extraCheese) {
+    public Sandwich(String name, SandwichSize size, BreadType breadType, boolean isToasted) {
         super(name, 0);
         this.size = size;
         this.breadType = breadType;
@@ -25,7 +25,6 @@ public class Sandwich extends Product{
         this.sauces = new ArrayList<>();
         this.isToasted = isToasted;
         this.extraMeat = false;
-        this.extraCheese = extraCheese;
 
         if (size == SandwichSize.FOUR_INCH) {
             this.price = 5.50;
@@ -36,6 +35,27 @@ public class Sandwich extends Product{
         }
 
     }
+
+    public Sandwich(String name, SandwichSize size, BreadType breadType,
+                    boolean isToasted, List<IncludedTopping> includedTopping, List<OtherTopping> otherTopping, List<Sauce> sauce) {
+        super(name, 0);
+        this.size = size;
+        this.breadType = breadType;
+        this.includedTopping = includedTopping;
+        this.otherTopping = otherTopping;
+        this.isToasted = isToasted;
+        this.sauces = sauce;
+        this.extraMeat = false;
+
+        if (size == SandwichSize.FOUR_INCH) {
+            this.price = 5.50;
+        } else if (size == SandwichSize.EIGHT_INCH) {
+            this.price = 7.00;
+        } else if (size == SandwichSize.TWELVE_INCH) {
+            this.price = 8.50;
+        }
+    }
+
 
     public SandwichSize getSize() {
         return size;
@@ -65,11 +85,83 @@ public class Sandwich extends Product{
         return extraCheese;
     }
 
+    public void addRegularTopping(IncludedTopping topping) {
+        includedTopping.add(topping);
+    }
+
+    public void addPremiumTopping(OtherTopping topping) {
+        otherTopping.add(topping);
+    }
+
+    public void addSauce(Sauce sauce) {
+        sauces.add(sauce);
+    }
+
+    public void setExtraMeat(boolean extraMeat) {
+        this.extraMeat = extraMeat;
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        this.extraCheese = extraCheese;
+    }
     @Override
     public double calculateProductTotal() {
         double total = price;
         for (OtherTopping topping : otherTopping) {
-            if (topping.getType() == OtherTopping.Type.MEAT)
+            if (topping.getType() == OtherTopping.Type.MEAT) {
+                if (size == SandwichSize.FOUR_INCH) {
+                    total += 1.00;
+                } else if (size == SandwichSize.EIGHT_INCH) {
+                    total += 2.00;
+                } else if (size == SandwichSize.TWELVE_INCH) {
+                    total += 3.00;
+                }
+            }
+
+            else if (topping.getType() == OtherTopping.Type.CHEESE) {
+                if (size == SandwichSize.FOUR_INCH) {
+                    total += 0.75;
+                } else if (size == SandwichSize.EIGHT_INCH) {
+                    total += 1.50;
+                } else if (size == SandwichSize.TWELVE_INCH) {
+                    total += 2.25;
+                }
+            }
         }
+
+        if (extraMeat) {
+            if (size == SandwichSize.FOUR_INCH) {
+                total += 0.30;
+            } else if (size == SandwichSize.EIGHT_INCH) {
+                total += 0.60;
+            } else if (size == SandwichSize.TWELVE_INCH) {
+                total += 0.90;
+            }
+        }
+
+        return total;
+    }
+
+    @Override
+    public String productDetails() {
+        StringBuilder details = new StringBuilder(name + "    " + String.format("$%.2f", calculateProductTotal()) + " \n"
+                + size + "\n    Bread Type: " + breadType + "\n    Toasted: " + isToasted +  "\n    Regular Toppings: ");
+        for (IncludedTopping topping : includedTopping) {
+            details.append(topping).append(", ");
+        }
+        details.append("\n    Premium Toppings: ");
+        for (OtherTopping topping : otherTopping) {
+            details.append(topping).append(", ");
+        }
+        if(extraMeat) {
+            details.append("\n    Extra Meat: Yes");
+        } else {
+            details.append("\n    Extra Meat: No");
+        }
+        details.append("\n    Sauces: ");
+        for (Sauce sauce : sauces) {
+            details.append(sauce).append(", ");
+        }
+        return details.toString();
     }
 }
